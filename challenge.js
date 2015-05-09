84bf42c1e46b40a5fedde1511c5c6eda48252c42
Ariel Verber <opiedesigns@gmail.com>
2015-05-09 13:47:53 +0300

    finished game logics

---
+++ challenge.js
@@ -0,0 +1,74 @@
+var currentId, currentRow, currentCol, numLeft = 20;
+var didStart = false;
+
+
+$(".square").click(function() {
+  if(!didStart){
+  currentId = $(this).attr('id'),
+    currentRow = currentId.charAt(2),
+    currentCol = currentId.charAt(3);
+  addToChosen();
+  }
+});
+
+$(".square").hover(function() {
+  if(!didStart){
+    $(this).addClass("selected");
+  }
+},
+function() {
+  if(!didStart){
+    $(this).removeClass("selected");
+  }
+}
+);
+
+$(window).keypress(function(e) {
+  if(didStart){
+    var key = e.which;
+    //right
+    if(key == 100){
+      currentCol++;
+    }
+    //left
+    else if(key == 97){
+      currentCol--;
+    }
+    //up
+    else if(key == 119){
+      currentRow--;
+    }
+    //down
+    else if(key == 115){
+      currentRow++;
+    }
+    else{
+      return;
+    }
+
+    window.setTimeout(addToChosen(), 1000);
+  }
+});
+
+var addToChosen = function() {
+  var squareId = "#sq" + currentRow + currentCol;
+  if (didStart && (($(squareId).length == 0) || ($(squareId).hasClass("selected")))) {
+    youLose();
+  } else {
+    didStart = true;
+    $(squareId).addClass("selected");
+    numLeft--;
+  }
+  if(numLeft == 0){
+    youWin();
+  }
+
+}
+
+var youLose = function() {
+  alert('you lose :(');
+}
+
+var youWin = function() {
+  alert('you win! :)');
+}

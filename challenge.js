d314f3bacaf7ed67d957be7ef92ced35b3067dc9
Ariel Verber <opiedesigns@gmail.com>
2015-05-09 16:35:33 +0300

    added restart, added nice background transitions

--- challenge.js
+++ challenge.js
@@ -1,6 +1,5 @@
 var previousId, currentId, currentRow, currentCol, numLeft = 20, fadeOutIndex = 0;
-var didStart = false,
-  didEnd = false;
+var didStart = false, didEnd = false;
 
 $('.row-lost').children().hide();
 $('.row-won').children().hide();
@@ -10,6 +9,24 @@ $('.row-won').children().hide();
 jQuery.fn.reverse = function() {
   return this.pushStack(this.get().reverse(), arguments);
 };
+
+jQuery.fn.removeClassExcept = function (val) {
+    return this.each(function (index, el) {
+        var keep = val.split(" "),  // list we'd like to keep
+        reAdd = [],          // ones that should be re-added if found
+        $el = $(el);       // element we're working on
+        // look for which we re-add (based on them already existing)
+        for (var c = 0; c < keep.length; c++){
+          if ($el.hasClass(keep[c])) reAdd.push(keep[c]);
+        }
+
+        // drop all, and only add those confirmed as existing
+        $el
+          .removeClass()               // remove existing classes
+          .addClass(reAdd.join(' '));  // re-add the confirmed ones
+    });
+};
+
 
 //.addClass("current");
 
@@ -119,3 +136,24 @@ var fadeInElements = function(specificClass) {
   });
 }
 
+
+var showInstructions = function(){
+  alert();
+}
+
+
+var restart = function(){
+  numLeft = 20, fadeOutIndex = 0, didStart = false, didEnd = false;
+  $('.row-lost').children().hide();
+  $('.row-won').children().hide();
+  $('.row1').children().show();
+  $('.row2').children().show();
+  $('.row3').children().show();
+  $('.logo-lose').fadeOut();
+  $('.logo-background').fadeIn();
+  $('.square').removeClassExcept('square');
+}
+
+$(".restart").click(function(){
+  restart();
+});

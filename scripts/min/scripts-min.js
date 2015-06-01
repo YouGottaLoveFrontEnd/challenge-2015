f033b18a69ecf3834c64c824d519877ca15ab947
Elior Shalev Tabeka <eliors@wix.com>
2015-06-01 16:46:07 +0300

    Improve the Text Shadow in the Front End Scene

--- scripts/min/scripts-min.js
+++ scripts/min/scripts-min.js
@@ -1,1 +1,333 @@
-!function(t){function e(t,e){for(var o in e)e.hasOwnProperty(o)&&(t[o]=e[o]);return t}function o(e){var o=0,i=0;if(!e)var e=t.event;return e.pageX||e.pageY?(o=e.pageX,i=e.pageY):(e.clientX||e.clientY)&&(o=e.clientX+document.body.scrollLeft+document.documentElement.scrollLeft,i=e.clientY+document.body.scrollTop+document.documentElement.scrollTop),{x:o,y:i}}function i(t,e){var o=!0;return function(i){o&&(o=!1,setTimeout(function(){o=!0},e),t(i))}}function n(t,o){this.el=t,this.options=e({},this.options),e(this.options,o),this._init(),this._initEvents()}function s(){[].slice.call(document.querySelectorAll("img.tilt-effect")).forEach(function(t){new n(t,JSON.parse(t.getAttribute("data-tilt-options")))})}skrollr.init({forceHeight:!1,smoothScrolling:!1}),t.onload=function(){t.scrollTo(0,0)},Pace.on("done",function(){document.getElementById("scroll-down-hint").classList.remove("is-hidden")}),realshadow(document.getElementsByTagName("h1"),{type:"text",color:"100,100,100",length:6}),realshadow(document.getElementsByClassName("item-pen"),{type:"drop",color:"0,0,0",length:7,inverse:!0}),realshadow(document.getElementsByClassName("item-brush-white"),{type:"drop",color:"100,100,100",length:7,inverse:!0}),realshadow(document.getElementsByClassName("item-mouse"),{type:"drop",color:"50,50,50",length:7,inverse:!0}),realshadow(document.getElementsByClassName("item-css"),{type:"drop",length:5}),realshadow(document.getElementsByClassName("item-html"),{type:"drop",length:4}),realshadow(document.getElementsByClassName("item-js"),{type:"drop",length:5}),realshadow(document.getElementsByClassName("item-drupal"),{type:"drop",length:6}),realshadow(document.getElementsByClassName("logo"),{type:"drop",length:3}),realshadow(document.getElementsByClassName("credit-box"),{type:"drop",length:3});var r,a,l,m,p,c,d,h,g,u,f,v,y,x,E,w,I;E=function(t,e){return t.style.webkitTransform=e,t.style.transform=e},l=function(t,e,o){return t.setAttribute("style","-webkit-filter: blur("+e+"); filter: blur("+e+"); opacity: "+o+";")},I=t.innerWidth,w=t.innerHeight,p=I/2,c=w/2,g=.04*I,u=.04*w,f=30,x=1200,h=300,v=.75,m=8,d=1-v,a=document.getElementById("background-image-layer"),r=document.getElementById("scene-blur-layer"),y=function(t,e,o,i){var n,s,c;return c="perspective("+x+") translate3d("+g*o+"px, "+u*i+"px, "+g*o+"px)",E(a,c),n=Math.abs(t-p)/p*m+"px",s=1-d*(Math.abs(t-p)/p),l(r,n,s)},document.addEventListener("mousemove",function(e){var o,i,n,s;t.event&&(e=t.event),o=e.clientX,i=e.clientY,n=1-o/p,s=1-i/c,y(o,i,n,s)},!1);for(var b=0,B="webkit moz ms o".split(" "),W=t.requestAnimationFrame,N=t.cancelAnimationFrame,k,C=0;C<B.length&&(!W||!N);C++)k=B[C],W=W||t[k+"RequestAnimationFrame"],N=N||t[k+"CancelAnimationFrame"]||t[k+"CancelRequestAnimationFrame"];W&&N||(W=function(e,o){var i=(new Date).getTime(),n=Math.max(0,16-(i-b)),s=t.setTimeout(function(){e(i+n)},n);return b=i+n,s},N=function(e){t.clearTimeout(e)}),n.prototype.options={extraImgs:2,opacity:.7,bgfixed:!0,movement:{perspective:1e3,translateX:-10,translateY:-10,translateZ:20,rotateX:2,rotateY:2,rotateZ:0}},n.prototype._init=function(){this.tiltWrapper=document.createElement("div"),this.tiltWrapper.className="tilt",this.tiltImgBack=document.createElement("div"),this.tiltImgBack.className="tilt__back",this.tiltImgBack.style.backgroundImage="url("+this.el.src+")",this.tiltWrapper.appendChild(this.tiltImgBack),this.options.extraImgs<1?this.options.extraImgs=1:this.options.extraImgs>5&&(this.options.extraImgs=5),this.options.movement.perspective||(this.options.movement.perspective=0),this.imgElems=[];for(var t=0;t<this.options.extraImgs;++t){var e=document.createElement("div");e.className="tilt__front",e.style.backgroundImage="url("+this.el.src+")",e.style.opacity=this.options.opacity,this.tiltWrapper.appendChild(e),this.imgElems.push(e)}this.options.bgfixed||(this.imgElems.push(this.tiltImgBack),++this.options.extraImgs),this.el.parentNode.insertBefore(this.tiltWrapper,this.el),this.el.parentNode.removeChild(this.el),this.view={width:this.tiltWrapper.offsetWidth,height:this.tiltWrapper.offsetHeight}},n.prototype._initEvents=function(){var e=this,n=e.options.movement;this.tiltWrapper.addEventListener("mousemove",function(t){W(function(){for(var i=o(t),s={left:document.body.scrollLeft+document.documentElement.scrollLeft,top:document.body.scrollTop+document.documentElement.scrollTop},r=e.tiltWrapper.getBoundingClientRect(),a={x:i.x-r.left-s.left,y:i.y-r.top-s.top},l=0,m=e.imgElems.length;m>l;++l){var p=e.imgElems[l],c=n.rotateX?2*((l+1)*n.rotateX/e.options.extraImgs)/e.view.height*a.y-(l+1)*n.rotateX/e.options.extraImgs:0,d=n.rotateY?2*((l+1)*n.rotateY/e.options.extraImgs)/e.view.width*a.x-(l+1)*n.rotateY/e.options.extraImgs:0,h=n.rotateZ?2*((l+1)*n.rotateZ/e.options.extraImgs)/e.view.width*a.x-(l+1)*n.rotateZ/e.options.extraImgs:0,g=n.translateX?2*((l+1)*n.translateX/e.options.extraImgs)/e.view.width*a.x-(l+1)*n.translateX/e.options.extraImgs:0,u=n.translateY?2*((l+1)*n.translateY/e.options.extraImgs)/e.view.height*a.y-(l+1)*n.translateY/e.options.extraImgs:0,f=n.translateZ?2*((l+1)*n.translateZ/e.options.extraImgs)/e.view.height*a.y-(l+1)*n.translateZ/e.options.extraImgs:0;p.style.WebkitTransform="perspective("+n.perspective+"px) translate3d("+g+"px,"+u+"px,"+f+"px) rotate3d(1,0,0,"+c+"deg) rotate3d(0,1,0,"+d+"deg) rotate3d(0,0,1,"+h+"deg)",p.style.transform="perspective("+n.perspective+"px) translate3d("+g+"px,"+u+"px,"+f+"px) rotate3d(1,0,0,"+c+"deg) rotate3d(0,1,0,"+d+"deg) rotate3d(0,0,1,"+h+"deg)"}})}),this.tiltWrapper.addEventListener("mouseleave",function(t){setTimeout(function(){for(var t=0,o=e.imgElems.length;o>t;++t){var i=e.imgElems[t];i.style.WebkitTransform="perspective("+n.perspective+"px) translate3d(0,0,0) rotate3d(1,1,1,0deg)",i.style.transform="perspective("+n.perspective+"px) translate3d(0,0,0) rotate3d(1,1,1,0deg)"}},60)}),t.addEventListener("resize",i(function(t){e.view={width:e.tiltWrapper.offsetWidth,height:e.tiltWrapper.offsetHeight}},50))},s(),t.TiltFx=n}(window);
+// *********************************************************/
+// *** Split the title for the Realshadow text effect ******/
+// *********************************************************/
+
+var string="Front End";
+var t = string.split('');
+
+for (var i = 0; i < t.length; i++) {
+	$(".title-frontend").append('<span class="letter">' + t[i] + '</span>' );
+}
+
+$(".letter").realshadow({type: 'text', opacity: 0.1, color: '177, 175, 171'});
+
+// *********************************************************/
+// *** Realshadow effect on items **************************/
+// *********************************************************/
+
+$(".item-pen").realshadow({type: 'drop', color: '0,0,0', length: 5, inverse: true});
+$(".item-brush-white").realshadow({type: 'drop', color: '100,100,100', length: 5, inverse: true});
+$(".item-mouse").realshadow({type: 'drop', color: '50,50,50', length: 5, inverse: true});
+$(".item-css").realshadow({type: 'drop', color: '50,50,50', length: 5, inverse: true});
+$(".item-html").realshadow({type: 'drop', length: 4});
+$(".item-js").realshadow({type: 'drop', length: 5});
+$(".item-drupal").realshadow({type: 'drop', length: 6});
+$(".logo").realshadow({type: 'drop', length: 3});
+$(".credit-box").realshadow({type: 'drop', length: 3});
+
+// *********************************************************/
+// *** Parallax slider *************************************/
+// *********************************************************/
+
+skrollr.init({
+	forceHeight: false,
+	smoothScrolling: false
+});
+
+// *********************************************************/
+// *** Scroll page to top onload ***************************/
+// *********************************************************/
+
+window.onload = function() {
+	window.scrollTo(0,0);
+};
+
+// *********************************************************/
+// *** Display the arrow scroll down hint after loading ****/
+// *********************************************************/
+
+Pace.on('done', function(){
+	document.getElementById('scroll-down-hint').classList.remove('is-hidden');
+});
+
+// *********************************************************/
+// *** Blur background Effect on mouse Movement ************/
+// *********************************************************/
+
+var $blur_layer, $image_layer, blur_fade, blur_throttle, center_x, center_y, fade_length, max_blur, max_x, max_y, max_z, min_opacity, mouse_move_handler, perspective, transform, win_h, win_w;
+transform = function(element, transform) {
+	element.style.webkitTransform = transform;
+	return element.style.transform = transform;
+};
+blur_fade = function(element, blur, fade) {
+	return element.setAttribute('style', '-webkit-filter: blur(' + blur + '); filter: blur(' + blur + '); opacity: ' + fade + ';');
+};
+win_w = window.innerWidth;
+win_h = window.innerHeight;
+center_x = win_w / 2;
+center_y = win_h / 2;
+max_x = win_w * 0.04;
+max_y = win_h * 0.04;
+max_z = 30;
+perspective = 1200;
+max_blur = 300;
+min_opacity = 0.75;
+blur_throttle = 8;
+fade_length = 1 - min_opacity;
+$image_layer = document.getElementById('background-image-layer');
+$blur_layer = document.getElementById('scene-blur-layer');
+
+mouse_move_handler = function(mouse_x, mouse_y, ratio_x, ratio_y) {
+	var blur_px, fade_i, transform_val;
+	transform_val = 'perspective(' + perspective + ') translate3d(' + (max_x * ratio_x) + 'px, ' + (max_y * ratio_y) + 'px, ' + (max_x * ratio_x) + 'px)';
+	transform($image_layer, transform_val);
+	blur_px = (Math.abs(mouse_x - center_x) / center_x * blur_throttle) + 'px';
+	fade_i = 1 - (fade_length * (Math.abs(mouse_x - center_x) / center_x));
+	return blur_fade($blur_layer, blur_px, fade_i);
+};
+
+document.addEventListener('mousemove', (function(event) {
+	var mouse_x, mouse_y, ratio_x, ratio_y;
+	if (window.event) {
+		event = window.event;
+	}
+	mouse_x = event.clientX;
+	mouse_y = event.clientY;
+	ratio_x = 1 - (mouse_x / center_x);
+	ratio_y = 1 - (mouse_y / center_y);
+	mouse_move_handler(mouse_x, mouse_y, ratio_x, ratio_y);
+}), false);
+
+
+// *********************************************************/
+// *** Tilt background Effect on mouse Movement ************/
+// *** from https://gist.github.com/desandro/1866474 *******/
+// *********************************************************/
+
+var lastTime = 0;
+var prefixes = 'webkit moz ms o'.split(' ');
+// get unprefixed rAF and cAF, if present
+var requestAnimationFrame = window.requestAnimationFrame;
+var cancelAnimationFrame = window.cancelAnimationFrame;
+// loop through vendor prefixes and get prefixed rAF and cAF
+var prefix;
+for( var i = 0; i < prefixes.length; i++ ) {
+	if ( requestAnimationFrame && cancelAnimationFrame ) {
+		break;
+	}
+	prefix = prefixes[i];
+	requestAnimationFrame = requestAnimationFrame || window[ prefix + 'RequestAnimationFrame' ];
+	cancelAnimationFrame  = cancelAnimationFrame  || window[ prefix + 'CancelAnimationFrame' ] ||
+	window[ prefix + 'CancelRequestAnimationFrame' ];
+}
+
+// fallback to setTimeout and clearTimeout if either request/cancel is not supported
+if ( !requestAnimationFrame || !cancelAnimationFrame ) {
+	requestAnimationFrame = function( callback, element ) {
+		var currTime = new Date().getTime();
+		var timeToCall = Math.max( 0, 16 - ( currTime - lastTime ) );
+		var id = window.setTimeout( function() {
+			callback( currTime + timeToCall );
+		}, timeToCall );
+		lastTime = currTime + timeToCall;
+		return id;
+	};
+
+	cancelAnimationFrame = function( id ) {
+		window.clearTimeout( id );
+	};
+}
+
+function extend( a, b ) {
+	for( var key in b ) {
+		if( b.hasOwnProperty( key ) ) {
+			a[key] = b[key];
+		}
+	}
+	return a;
+}
+
+// from http://www.quirksmode.org/js/events_properties.html#position
+function getMousePos(e) {
+	var posx = 0;
+	var posy = 0;
+	if (!e) var e = window.event;
+	if (e.pageX || e.pageY) 	{
+		posx = e.pageX;
+		posy = e.pageY;
+	}
+	else if (e.clientX || e.clientY) 	{
+		posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+		posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+	}
+	return {
+		x : posx,
+		y : posy
+	}
+}
+
+// from http://www.sberry.me/articles/javascript-event-throttling-debouncing
+function throttle(fn, delay) {
+	var allowSample = true;
+
+	return function(e) {
+		if (allowSample) {
+			allowSample = false;
+			setTimeout(function() { allowSample = true; }, delay);
+			fn(e);
+		}
+	};
+}
+
+/***************************************************************************/
+
+/**
+ * TiltFx fn
+ */
+function TiltFx(el, options) {
+	this.el = el;
+	this.options = extend( {}, this.options );
+	extend( this.options, options );
+	this._init();
+	this._initEvents();
+}
+
+/**
+ * TiltFx options.
+ */
+TiltFx.prototype.options = {
+	// number of extra image elements (div with background-image) to add to the DOM - min:1, max:5 (for a higher number, it's recommended to remove the transitions of .tilt__front in the stylesheet.
+	extraImgs : 2,
+	// the opacity value for all the image elements.
+	opacity : 0.7,
+	// by default the first layer does not move.
+	bgfixed : true,
+	// image element's movement configuration
+	movement : {
+		perspective : 1000, // perspective value
+		translateX : -10, // a relative movement of -10px to 10px on the x-axis (setting a negative value reverses the direction)
+		translateY : -10, // a relative movement of -10px to 10px on the y-axis
+		translateZ : 20, // a relative movement of -20px to 20px on the z-axis (perspective value must be set). Also, this specific translation is done when the mouse moves vertically.
+		rotateX : 2, // a relative rotation of -2deg to 2deg on the x-axis (perspective value must be set)
+		rotateY : 2, // a relative rotation of -2deg to 2deg on the y-axis (perspective value must be set)
+		rotateZ : 0 // z-axis rotation; by default there's no rotation on the z-axis (perspective value must be set)
+	}
+}
+
+/**
+ * Initialize: build the necessary structure for the image elements and replace it with the HTML img element.
+ */
+TiltFx.prototype._init = function() {
+	this.tiltWrapper = document.createElement('div');
+	this.tiltWrapper.className = 'tilt';
+
+	// main image element.
+	this.tiltImgBack = document.createElement('div');
+	this.tiltImgBack.className = 'tilt__back';
+	this.tiltImgBack.style.backgroundImage = 'url(' + this.el.src + ')';
+	this.tiltWrapper.appendChild(this.tiltImgBack);
+
+	// image elements limit.
+	if( this.options.extraImgs < 1 ) {
+		this.options.extraImgs = 1;
+	}
+	else if( this.options.extraImgs > 5 ) {
+		this.options.extraImgs = 5;
+	}
+
+	if( !this.options.movement.perspective ) {
+		this.options.movement.perspective = 0;
+	}
+
+	// add the extra image elements.
+	this.imgElems = [];
+	for(var i = 0; i < this.options.extraImgs; ++i) {
+		var el = document.createElement('div');
+		el.className = 'tilt__front';
+		el.style.backgroundImage = 'url(' + this.el.src + ')';
+		el.style.opacity = this.options.opacity;
+		this.tiltWrapper.appendChild(el);
+		this.imgElems.push(el);
+	}
+
+	if( !this.options.bgfixed ) {
+		this.imgElems.push(this.tiltImgBack);
+		++this.options.extraImgs;
+	}
+
+	// add it to the DOM and remove original img element.
+	this.el.parentNode.insertBefore(this.tiltWrapper, this.el);
+	this.el.parentNode.removeChild(this.el);
+
+	// tiltWrapper properties: width/height/left/top
+	this.view = { width : this.tiltWrapper.offsetWidth, height : this.tiltWrapper.offsetHeight };
+};
+
+/**
+ * Initialize the events on the main wrapper.
+ */
+TiltFx.prototype._initEvents = function() {
+	var self = this,
+		moveOpts = self.options.movement;
+
+	// mousemove event..
+	this.tiltWrapper.addEventListener('mousemove', function(ev) {
+		requestAnimationFrame(function() {
+			// mouse position relative to the document.
+			var mousepos = getMousePos(ev),
+			// document scrolls.
+				docScrolls = {left : document.body.scrollLeft + document.documentElement.scrollLeft, top : document.body.scrollTop + document.documentElement.scrollTop},
+				bounds = self.tiltWrapper.getBoundingClientRect(),
+			// mouse position relative to the main element (tiltWrapper).
+				relmousepos = {
+					x : mousepos.x - bounds.left - docScrolls.left,
+					y : mousepos.y - bounds.top - docScrolls.top
+				};
+
+			// configure the movement for each image element.
+			for(var i = 0, len = self.imgElems.length; i < len; ++i) {
+				var el = self.imgElems[i],
+					rotX = moveOpts.rotateX ? 2 * ((i+1)*moveOpts.rotateX/self.options.extraImgs) / self.view.height * relmousepos.y - ((i+1)*moveOpts.rotateX/self.options.extraImgs) : 0,
+					rotY = moveOpts.rotateY ? 2 * ((i+1)*moveOpts.rotateY/self.options.extraImgs) / self.view.width * relmousepos.x - ((i+1)*moveOpts.rotateY/self.options.extraImgs) : 0,
+					rotZ = moveOpts.rotateZ ? 2 * ((i+1)*moveOpts.rotateZ/self.options.extraImgs) / self.view.width * relmousepos.x - ((i+1)*moveOpts.rotateZ/self.options.extraImgs) : 0,
+					transX = moveOpts.translateX ? 2 * ((i+1)*moveOpts.translateX/self.options.extraImgs) / self.view.width * relmousepos.x - ((i+1)*moveOpts.translateX/self.options.extraImgs) : 0,
+					transY = moveOpts.translateY ? 2 * ((i+1)*moveOpts.translateY/self.options.extraImgs) / self.view.height * relmousepos.y - ((i+1)*moveOpts.translateY/self.options.extraImgs) : 0,
+					transZ = moveOpts.translateZ ? 2 * ((i+1)*moveOpts.translateZ/self.options.extraImgs) / self.view.height * relmousepos.y - ((i+1)*moveOpts.translateZ/self.options.extraImgs) : 0;
+
+				el.style.WebkitTransform = 'perspective(' + moveOpts.perspective + 'px) translate3d(' + transX + 'px,' + transY + 'px,' + transZ + 'px) rotate3d(1,0,0,' + rotX + 'deg) rotate3d(0,1,0,' + rotY + 'deg) rotate3d(0,0,1,' + rotZ + 'deg)';
+				el.style.transform = 'perspective(' + moveOpts.perspective + 'px) translate3d(' + transX + 'px,' + transY + 'px,' + transZ + 'px) rotate3d(1,0,0,' + rotX + 'deg) rotate3d(0,1,0,' + rotY + 'deg) rotate3d(0,0,1,' + rotZ + 'deg)';
+			}
+		});
+	});
+
+	// reset all when mouse leaves the main wrapper.
+	this.tiltWrapper.addEventListener('mouseleave', function(ev) {
+		setTimeout(function() {
+			for(var i = 0, len = self.imgElems.length; i < len; ++i) {
+				var el = self.imgElems[i];
+				el.style.WebkitTransform = 'perspective(' + moveOpts.perspective + 'px) translate3d(0,0,0) rotate3d(1,1,1,0deg)';
+				el.style.transform = 'perspective(' + moveOpts.perspective + 'px) translate3d(0,0,0) rotate3d(1,1,1,0deg)';
+			}
+		}, 60);
+
+	});
+
+	// window resize
+	window.addEventListener('resize', throttle(function(ev) {
+		// recalculate tiltWrapper properties: width/height/left/top
+		self.view = { width : self.tiltWrapper.offsetWidth, height : self.tiltWrapper.offsetHeight };
+	}, 50));
+};
+
+function init() {
+	// search for imgs with the class "tilt-effect"
+	[].slice.call(document.querySelectorAll('img.tilt-effect')).forEach(function(img) {
+		new TiltFx(img, JSON.parse(img.getAttribute('data-tilt-options')));
+	});
+}
+
+init();
+
+window.TiltFx = TiltFx;
+
+
